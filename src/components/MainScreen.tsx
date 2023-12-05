3ee5c297710424d04b58d24b57798cfef14024cb
Hayeon-Lee <papepopepe@naver.com>
2023-12-06 00:19:59 +0900

    임시 커밋

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,28 +1,31 @@
-import { useState } from "react";
-import { userService } from "../services/userService";
-import * as React from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from 'react';
+import { userService } from '../services/userService';
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const MainScreen = () => {
-    const us = new userService();
-    const navigate = useNavigate();
+  const us = new userService();
+  const navigate = useNavigate();
 
-    const logout = async () => {
-       const logoutInfo = await us.deleteInfo();
-       if(logoutInfo === true) {
-        alert("성공적으로 로그아웃 됐습니다.");
-        navigate('/');
-       }
+  const logout = async () => {
+    const logoutInfo = await us.deleteInfo();
+    if (logoutInfo === true) {
+      alert('성공적으로 로그아웃 됐습니다.');
+      navigate('/');
     }
+  };
 
-    return (
-        <div className="Container">
-            <div className="Wrapper">
-            <img className ="logoImg" src='img/logo.png'/>
-            <button type='button' onClick={logout}>로그아웃하기</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className='Container'>
+      <div className='Wrapper'>
+        <img className='logoImg' src='img/logo.png' />
+        <button type='button' onClick={logout}>
+          로그아웃하기
+        </button>
+        <span>Hello!</span>
+      </div>
+    </div>
+  );
+};
 
 export default MainScreen;
